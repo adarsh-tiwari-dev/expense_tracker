2e13e80b54ee7bfc8ada3c6b22fc47389ffe2c1a
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-03-31 15:35:27 +0530

    Switched Database from SQlite to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'expense_tracker_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
