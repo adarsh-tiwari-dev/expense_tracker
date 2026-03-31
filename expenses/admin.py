3c0c30499fa81da859e28de9b2938d4b3e403fef
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-03-31 14:41:29 +0530

    Admin panel setup with Expense customization

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Expense
 # Register your models here.
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'category', 'date', 'created_at')
+    list_filter = ('category', 'date')
+    search_fields = ('category', 'description')
+    ordering = ('-date',)
