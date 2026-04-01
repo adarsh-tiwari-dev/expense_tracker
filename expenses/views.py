cb0c3241e7bb25e2c560374728b20764840c0065
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-04-01 11:27:33 +0530

    Added search and ordering to Expense API

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Expense
 from .serializers import ExpenseSerializer
@@ -9,6 +9,12 @@ from .serializers import ExpenseSerializer
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+        ]
     filterset_fields = ['category','date']
-    
+    search_fields = ['category','description']
+    ordering_fields = ['amount','date','created_at']
+    ordering = ['-date']
