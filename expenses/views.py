f2a41bc289dd61bea8fe15cd787f69240e59f3ff
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-04-01 10:46:40 +0530

    Day 2 Implemented CRUD API using DRF ViewSet and Router

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense
+from .serializers import ExpenseSerializer
 
 # Create your views here.
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
