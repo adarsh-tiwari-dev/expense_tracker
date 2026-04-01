c2703a8be3cb3c8d485e7cbb0c17b994c745dfa1
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-04-01 11:07:57 +0530

    Added filtering and pagination to Expense API

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Expense
 from .serializers import ExpenseSerializer
 
@@ -8,3 +9,6 @@ from .serializers import ExpenseSerializer
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category','date']
+    
