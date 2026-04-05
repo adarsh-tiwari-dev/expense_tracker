1bbd3dc66f5ab7ba2ded06c4d1b2e55ac9d55c63
Adarsh Tiwari <adarshtiwari01232004@gmail.com~>
2026-04-05 15:00:09 +0530

    Added budget model and overspending detection

--- expenses/models.py
+++ expenses/models.py
@@ -13,3 +13,14 @@ class Expense(models.Model):
 
     def __str__(self):
         return f"{self.category} - {self.amount}"
+
+
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    limit = models.DecimalField(max_digits=10,decimal_places=2)
+    month = models.DateField()
+
+
+    def __str__(self):
+        return f"{self.category} - {self.limit}"
